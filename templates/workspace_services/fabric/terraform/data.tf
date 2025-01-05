e4ecbb04b1ea42bbae49bccccdf192bd11916bb9
Ron Shakutai <ronshakutai@microsoft.com>
2025-01-05 13:19:31 +0000

    bump version to 1.0.40 and update resource group references in Terraform configuration

--- templates/workspace_services/fabric/terraform/data.tf
+++ templates/workspace_services/fabric/terraform/data.tf
@@ -4,13 +4,13 @@ data "azurerm_resource_group" "ws" {
 
 data "azurerm_virtual_network" "ws" {
   name                = local.virtual_network_name
-  resource_group_name = data.azurerm_resource_group.ws.name
+  resource_group_name = local.workspace_resource_group_name
 }
 
 data "azurerm_subnet" "services" {
   name                 = "ServicesSubnet"
-  virtual_network_name = data.azurerm_virtual_network.ws.name
-  resource_group_name  = data.azurerm_virtual_network.ws.resource_group_name
+  virtual_network_name = local.virtual_network_name
+  resource_group_name  = local.workspace_resource_group_name
 }
 
 data "azurerm_client_config" "current" {}
