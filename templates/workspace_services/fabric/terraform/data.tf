876be0bbb337b9a648f978c6dc744cc6643c5927
Ron Shakutai <ronshakutai@microsoft.com>
2025-01-05 10:55:50 +0000

    pr changes

--- templates/workspace_services/fabric/terraform/data.tf
+++ templates/workspace_services/fabric/terraform/data.tf
@@ -7,11 +7,6 @@ data "azurerm_virtual_network" "ws" {
   resource_group_name = data.azurerm_resource_group.ws.name
 }
 
-data "azurerm_firewall" "firewall" {
-  name                = local.firewall_name
-  resource_group_name = local.core_resource_group_name
-}
-
 data "azurerm_subnet" "services" {
   name                 = "ServicesSubnet"
   virtual_network_name = data.azurerm_virtual_network.ws.name
--- templates/workspace_services/fabric/terraform/outputs.tf
+++ templates/workspace_services/fabric/terraform/outputs.tf
@@ -1,23 +1,3 @@
-output "analysis_dns_zone_id" {
-  value = azurerm_private_dns_zone.dns_zone["analysis"].id
-}
-
-output "pbidedicated_dns_zone_id" {
-  value = azurerm_private_dns_zone.dns_zone["pbidedicated"].id
-}
-
-output "powerquery_dns_zone_id" {
-  value = azurerm_private_dns_zone.dns_zone["powerquery"].id
-}
-
-output "fabric_private_endpoint_name" {
-  value = azurerm_private_endpoint.fabric_pbi.name
-}
-
-output "pbi_privatelink_id" {
-  value = azapi_resource.pbi_privatelink.id
-}
-
 output "workspace_address_space" {
-  value = jsonencode(data.azurerm_virtual_network.ws.address_space)
+  value = jsonencode(data.azurerm_subnet.services.address_prefix)
 }
